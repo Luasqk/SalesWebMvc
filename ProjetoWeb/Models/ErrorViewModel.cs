769628a0fbe477067dae0d3fed95bc173fb97f64
Lucas Brandao <palarobrandaolucas@edu.unifil.br>
2026-06-05 10:29:50 -0300

    project

---
+++ ProjetoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace ProjetoWeb.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
