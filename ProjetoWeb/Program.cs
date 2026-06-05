412d053f0c09b9bfcc5342bf51661b5627675eb8
Lucas Brandao <palarobrandaolucas@edu.unifil.br>
2026-06-05 14:39:41 -0300

    arrumando a bagunca da estrutura, drivers, pastas e /departments

--- ProjetoWeb/Program.cs
+++ ProjetoWeb/Program.cs
@@ -1,8 +1,19 @@
 using Microsoft.EntityFrameworkCore;
-var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ProjetoWebContext") ?? throw new InvalidOperationException("Connection string 'ProjetoWebContext' not found.");
 
-builder.Services.AddDbContext<ProjetoWebContext>(options => options.UseSqlServer(connectionString));
+var builder = WebApplication.CreateBuilder(args);
+
+//pega a Connection String correta do appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("ProjetoWebContext")
+    ?? throw new InvalidOperationException("Connection string 'ProjetoWebContext' not found.");
+
+//configura o DbContext usando o MySql (Pomelo)
+builder.Services.AddDbContext<ProjetoWebContext>(options =>
+    options.UseMySql(
+        connectionString,
+        ServerVersion.AutoDetect(connectionString),
+        builder => builder.MigrationsAssembly("ProjetoWeb")
+    )
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -10,23 +21,20 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
+if (!app.Environment.IsDevelopment())
+{
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
