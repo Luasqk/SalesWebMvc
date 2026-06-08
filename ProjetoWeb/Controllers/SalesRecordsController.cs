bb1872648c9ca9fce720f23a175a523c5fbb85b5
Lucas Brandao <palarobrandaolucas@edu.unifil.br>
2026-06-08 15:59:17 -0300

    simple search

--- ProjetoWeb/Controllers/SalesRecordsController.cs
+++ ProjetoWeb/Controllers/SalesRecordsController.cs
@@ -1,20 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoWeb.Services;
 
 namespace ProjetoWeb.Controllers
 {
     public class SalesRecordsController : Controller
     {
+
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
-        public IActionResult GroupingSearch()
+        public IActionResult Gr()
         {
             return View();
         }
