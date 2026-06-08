9c12f2c97be2996b8b00f0423fbd19b69e490551
Lucas Brandao <palarobrandaolucas@edu.unifil.br>
2026-06-08 11:51:42 -0300

    update seller and custom exception

--- ProjetoWeb/Controllers/SellersController.cs
+++ ProjetoWeb/Controllers/SellersController.cs
@@ -64,5 +64,42 @@ namespace ProjetoWeb.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+            return View(obj);
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+
     }
 }
---
+++ ProjetoWeb/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjetoWeb.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException(string message) : base(message) { }
+    }
+}
